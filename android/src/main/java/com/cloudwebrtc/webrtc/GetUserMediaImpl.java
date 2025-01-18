7940234aa5c21fa7b7b48df530dad10f92801af9
abhixdeku <tabhishekpaul@gmail.com>
2025-01-18 16:41:45 +0530

    Handle CameraUtils initialization error when activity is missing for device orientation

--- android/src/main/java/com/cloudwebrtc/webrtc/GetUserMediaImpl.java
+++ android/src/main/java/com/cloudwebrtc/webrtc/GetUserMediaImpl.java
@@ -187,8 +187,7 @@ public class GetUserMediaImpl {
                         "Can't run requestStart() due to a low API level. API level 21 or higher is required.");
                 return;
             } else {
-                MediaProjectionManager mediaProjectionManager =
-                        (MediaProjectionManager) activity.getSystemService(Context.MEDIA_PROJECTION_SERVICE);
+                MediaProjectionManager mediaProjectionManager = (MediaProjectionManager) activity.getSystemService(Context.MEDIA_PROJECTION_SERVICE);
 
                 // call for the projection manager
                 this.startActivityForResult(
